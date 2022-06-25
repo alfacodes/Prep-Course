09c9eb91c7f39e62442ecb77739d799c24697754
franco <fialarcon@hotmail.com>
2022-06-25 20:16:05 -0300

    fixed mistake again

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -166,11 +166,13 @@ function esPositivo(numero) {
   //Si el número es positivo, devolver ---> "Es positivo"
   //Si el número es negativo, devolver ---> "Es negativo"
   //Si el número es 0, devuelve false
-  if(numero === 0){
+  if(numero === 0) {
     return false;
-  } else if(numero > 0) {
+  } 
+  else if(numero > 0) {
     return "Es positivo";
-  } else(numero < 0) {
+  } 
+  else{
     return "Es negativo";
   }
 }
